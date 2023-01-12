93d1d3bdb3b77cfee302116a9b3e0b3a8d9c418c
RaimondoReggio <reggioraimondo7@gmail.com>
2023-01-12 16:18:49 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(
     cors({
-      origin: 'https://talk-to-learn.vercel.app/',
+      origin: 'https://talk-to-learn.onrender.com',
       methods: ["GET", "POST"],
       credentials: true,
     })
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 
 const io = socket(server,{
     cors:{
-       origin: 'https://talk-to-learn.vercel.app/',
+       origin: 'https://talk-to-learn.onrender.com',
         Credential:true,     
     }}
 );
